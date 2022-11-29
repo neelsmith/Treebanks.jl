db0a17bf2c56c0c6a1814c5d6c4026f4acb4903c
Neel Smith <dnsmith.neel@gmail.com>
2022-11-29 08:33:56 -0500

    Outline parsing algorithm

--- src/graphs.jl
+++ src/graphs.jl
@@ -1,3 +1,16 @@
+function graph(s::Sentence)
+    g = DiGraph(length(s.words))
+    #=
+    for e in gr.edges
+        # findfirst
+        node1 = personindex(e.p1, gr.nodes)
+        node2 = personindex(e.p2, gr.nodes)
+        add_edge!(g, node1, node2)
+    end
+    =#
+    g
+end
+
 #=
 function graph(gr::GenealogyGraph)
     g = DiGraph(length(gr.nodes))
