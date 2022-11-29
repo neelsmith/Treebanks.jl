734e824bc14b3eb81ba04745e5b3a80bd98801b8
Neel Smith <dnsmith.neel@gmail.com>
2022-11-29 17:44:21 -0500

    Add some navigation with docstrings

--- docs/src/index.md
+++ docs/src/index.md
@@ -7,14 +7,52 @@ f = joinpath(root, "test", "data", "0540-001.xml")
 
 The file `f` is a tree bank of Lysias 1, available in this repository from `test/data/0540-001.xml`.
 
-A tree bank is a sequence of `Sentence` objects.
 
-```@example datawork
+## Structures
+
+A tree bank is a vector of `Sentence` objects.
+
+```@example treebanks
 using Treebanks
 tb = readtreebank(f)
 typeof(tb)
 ```
 
-```@example datawork
-f
+Sentences have a CTS URN and a Vector of `ParsedWord`s.
+
+
+```@example treebanks
+sent = tb[15]
+sent.urn
+```
+
+```@example treebanks
+sent.words |> typeof
+```
+
+
+Parsed words look like this:
+
+```@example treebanks
+sent.words[3]
+```
+
+## Graphs
+
+Use the `graph` function to create a directed graph of words in a sentence.
+
+```@example treebanks
+gr = Treebanks.graph(sent)
+```
+
+Find the root of a graph:
+
+```@example treebanks
+headword = Treebanks.root(sent)
+```
+Find words dependent on another word.  (In terms of the directed graph, find the `ParsedWord`s that are source nodes, or in-links, to the target node.)
+
+
+```@example treebanks
+nexttier = Treebanks.dependentwords(headword, gr, sent)
 ```
--- src/graphs.jl
+++ src/graphs.jl
@@ -6,7 +6,7 @@ function idxforid(id, wordvect)
 end
 
 
-"""Construct a graph of words in sentence `s`.
+"""Construct a directed graph of words in sentence `s`.
 """
 function graph(s::Sentence)
     nodevals = map(s.words) do w
@@ -30,7 +30,33 @@ function graph(s::Sentence)
     g
 end
    
-function dependentwords(wordidx, gr, s)
+function dependentwords(wordidx, gr::T, s::Sentence)  where  T <: SimpleValueGraphs.ValDiGraph
     indexes = inneighbors(gr, wordidx)
     map(i -> s.words[i], indexes)
+end
+
+
+   
+function dependentwords(w::ParsedWord, gr::T, s::Sentence)  where  T <: SimpleValueGraphs.ValDiGraph
+    keyval = idxforid(w.id, s.words)
+    dependentwords(keyval, gr, s)
+end
+
+function rootidx(gr::T) where T <: SimpleValueGraphs.ValDiGraph
+    rootidx = -1
+    for v in vertices(gr)
+        if get_vertexval(gr, v, :parent) == 0
+            @debug("Found root index $(v).")
+            rootidx = v
+        end
+    end
+    rootidx
+end
+
+function root(gr::T, wordlist::Vector{ParsedWord}) where T <: SimpleValueGraphs.ValDiGraph
+    wordlist[rootidx(gr)]
+end
+
+function root(gr::T, s::Sentence) where T <: SimpleValueGraphs.ValDiGraph
+    root(gr, s.words)
 end
